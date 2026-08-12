8dddc33fe024da407c409ca632caba4bff8e25a8
yyyunniee-web <yyyunniee@gmail.com>
2026-08-12 22:17:03 +0800

    衣橱APP初始完整源码

---
+++ public/sw.js
@@ -0,0 +1,22 @@
+const CACHE = 'wardrobe-v1';
+const ASSETS = ['./', './index.html', './manifest.webmanifest', './icon.svg'];
+self.addEventListener('install', (e) => {
+  e.waitUntil(caches.open(CACHE).then((c) => c.addAll(ASSETS)).catch(() => {}));
+  self.skipWaiting();
+});
+self.addEventListener('activate', (e) => {
+  e.waitUntil(
+    caches.keys().then((keys) => Promise.all(keys.filter((k) => k !== CACHE).map((k) => caches.delete(k))))
+  );
+  self.clients.claim();
+});
+self.addEventListener('fetch', (e) => {
+  if (e.request.method !== 'GET') return;
+  e.respondWith(
+    caches.match(e.request).then((cached) => cached || fetch(e.request).then((res) => {
+      const copy = res.clone();
+      caches.open(CACHE).then((c) => c.put(e.request, copy)).catch(() => {});
+      return res;
+    }).catch(() => cached))
+  );
+});
